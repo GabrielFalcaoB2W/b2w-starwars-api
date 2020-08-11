212c0fc3821240e200c4f4549de9d8196605267d
Gabriel Falcão <thegabrielfalcao@gmail.com>
2020-08-11 01:54:28 -0300

    Alterações no controller, adição de testes, mudanças no orquestrador de exceptions

--- src/main/java/io/b2w/starwars/controller/PlanetController.java
+++ src/main/java/io/b2w/starwars/controller/PlanetController.java
@@ -2,8 +2,14 @@ package io.b2w.starwars.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,36 +22,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.b2w.starwars.model.Planet;
+import io.b2w.starwars.repository.PlanetRepository;
+import io.b2w.starwars.service.PlanetService;
 
 @RestController
-@RequestMapping("/api/planet")
+@RequestMapping("/api/planets")
 @CrossOrigin("*")
 public class PlanetController {
 
+	@Autowired
+	private PlanetRepository planetRepo;
+	
+	@Autowired
+	private PlanetService service;
+	
+	@Value("${io.b2w.starwars.path}")
+	private String path;
+	
+	@Value("${io.b2w.starwars.endpoint.planet}")
+	private String endpoint;
+	
 	@PostMapping
-	public ResponseEntity<Planet> create(@RequestBody Planet planet) throws URISyntaxException {
-		return ResponseEntity.created(new URI("/api/planet/".concat(planet.getId().toString()))).build();
+	public ResponseEntity<Planet> create(@Valid @RequestBody @NotNull Planet planet) throws URISyntaxException {
+		service.insert(planet);
+		return ResponseEntity.created(new URI(path.concat(endpoint.concat(planet.getId().toString())))).build();
 	}
 
 	@GetMapping
-	public ResponseEntity<List<Planet>> findAll() {
-		return null;
+	public ResponseEntity<Page<Planet>> find(
+			@RequestParam(name = "name", required = false) String name,
+			@RequestParam(name = "page", defaultValue = "0") Integer page, 
+			@RequestParam(name = "size", defaultValue = "5") Integer size
+			) {
+		return ResponseEntity.ok(service.find(name, PageRequest.of(page, size)));
 	}
 	
-	@GetMapping
-	public ResponseEntity<Planet> findByName(@RequestParam("name") String name) {
-		return null;
+	@GetMapping("/{id}")
+	public ResponseEntity<Planet> findById(@PathVariable("id") String id) {
+		return ResponseEntity.ok(service.findById(id));
 	}
 	
-	@GetMapping
-	@RequestMapping("/{id}")
-	public ResponseEntity<Planet> findById(@PathVariable("id") Long id) {
-		return null;
-	}
-	
-	@DeleteMapping
-	@RequestMapping("/{id}")
-	public ResponseEntity<Planet> remove(@PathVariable("id") Long id) {
-		return null;
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Planet> remove(@PathVariable("id") String id) {
+		planetRepo.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/io/b2w/starwars/model/Planet.java
+++ src/main/java/io/b2w/starwars/model/Planet.java
@@ -1,5 +1,13 @@
 package io.b2w.starwars.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,11 +19,18 @@ import lombok.Setter;
 @Setter
 @Getter
 @EqualsAndHashCode(of = "id")
+@Document(collection = "planets")
 public class Planet {
 
-	private Long id;
+	@Id
+	@JsonProperty(access = Access.READ_ONLY)
+	private String id;
+	
+	@NotBlank(message = "The attribute name is required")
 	private String name;
+	@NotBlank(message = "The attribute climate is required")
 	private String climate;
+	@NotBlank(message = "The attribute terrain is required")
 	private String terrain;
-	private Integer timesOnMovies;
+	private int timesOnMovies;
 }
